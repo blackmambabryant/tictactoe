6cf3206123f3d408fff571922b80eaeb510372d1
blackmambabryant <vishal.shangari@gmail.com>
2020-05-29 11:09:31 -0700

    Updates

---
+++ precache-manifest.3a40a63ca21d57aa4eca719f179c31c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34e627047e5ffd48665d7d10ed2c4765",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "0f92988a639afb705763",
+    "url": "/tictactoe/static/css/main.cab8f95a.chunk.css"
+  },
+  {
+    "revision": "32540ac14b0f9e67caa5",
+    "url": "/tictactoe/static/js/2.ac17b5d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.ac17b5d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f92988a639afb705763",
+    "url": "/tictactoe/static/js/main.a0a3c06f.chunk.js"
+  },
+  {
+    "revision": "5f8a470de2451e71dd20",
+    "url": "/tictactoe/static/js/runtime-main.59855f66.js"
+  }
+]);
